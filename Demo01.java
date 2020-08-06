63e2fefcaadd441cfb22dda5c2d2f39289272ad3
zahngsan <zhang@itcast.cn>
2020-08-06 13:07:23 +0800

    Demo01.java 的第三个版本

--- Demo01.java
+++ Demo01.java
@@ -1,5 +1,5 @@
 public class Demo01{
 	public static void main(String[] agrs){
-		System.out.println("I love China_1,I love itcast");
+		System.out.println("I love China_1,I love itcast,I love heima");
 	}
 }
